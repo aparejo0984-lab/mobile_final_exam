23cf0baeca345ab6d65e2ceb21ff98bbe638ac77
Anna Lou Parejo <aparejo0984@gmail.com>
2021-06-04 08:41:20 +0800

    Mobile Final Examination

---
+++ app/src/main/java/com/example/patternlock/MainActivity.java
@@ -0,0 +1,96 @@
+package com.example.patternlock;
+
+import android.os.Bundle;
+
+import com.andrognito.patternlockview.PatternLockView;
+import com.andrognito.patternlockview.listener.PatternLockViewListener;
+import com.andrognito.patternlockview.utils.PatternLockUtils;
+import com.andrognito.patternlockview.utils.ResourceUtils;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.util.Log;
+import android.view.View;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        PatternLockView patternLockView = findViewById(R.id.patternView);
+        patternLockView.addPatternLockListener(new PatternLockViewListener() {
+            @Override
+            public void onStarted() {
+
+            }
+
+            @Override
+            public void onProgress(List<PatternLockView.Dot> progressPattern) {
+
+            }
+
+            @Override
+            public void onComplete(List pattern) {
+                Log.d(getClass().getName(), "Pattern complete: " +
+                        PatternLockUtils.patternToString(patternLockView, pattern)
+                    );
+
+                if(PatternLockUtils.patternToString(patternLockView,pattern).equalsIgnoreCase("123")){
+                    patternLockView.setViewMode(PatternLockView.PatternViewMode.CORRECT);
+                    Toast.makeText(MainActivity.this, "Welcome back!!!", Toast.LENGTH_SHORT).show();
+                }else{
+                    patternLockView.setViewMode(PatternLockView.PatternViewMode.WRONG);
+                    Toast.makeText(MainActivity.this, "Incorrect password", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onCleared() {
+
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
